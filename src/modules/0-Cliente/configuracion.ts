d3bfadf4a4c26bf6c2e5abc8d0054549c130b1c3
@mpaez1 <mpaez@ikatechsolutions.com>
2024-09-13 23:06:51 -0500

    add-changes

---
+++ src/modules/0-Cliente/configuracion.ts
@@ -0,0 +1,7 @@
+// Configuración del navegador de la tienda
+export const titlePageStore = "Mi Empresa"
+export const iconNavegadorStore = "https://cdn-icons-png.flaticon.com/512/148/148735.png"
+export const descriptionStore = "Explora nuestra tienda en línea y encuentra productos únicos a precios competitivos. Compra ahora y disfruta de envío gratis."
+export const keywordsStore = "tienda online, productos, compras, envío gratis"
+export const urlStore = "https://far-fireball.vercel.app/"
+export const imageStore = "https://cdn-icons-png.flaticon.com/512/148/148735.png"
