c8ed452d0c3cb4cccaa5dc1f7c83e919e4f4bf45
@mpaez1 <mpaez@ikatechsolutions.com>
2024-09-15 23:56:08 -0500

    add-changes

--- src/modules/0-Cliente-Configuracion/1-intros-config/1-intros-config.jsx
+++ src/modules/0-Cliente-Configuracion/1-intros-config/1-intros-config.jsx
@@ -1,13 +1,5 @@
-// Que introducción va a estar activa
-export const acvtiveIntro1 = false
-export const acvtiveIntro2 = false
-export const acvtiveIntro3 = false
-export const acvtiveIntro5 = false
-export const acvtiveIntro6 = false
-export const acvtiveIntro7 = false
-export const acvtiveIntro8 = false
-export const acvtiveIntro9 = false
-export const acvtiveIntro10 = false
-
+// Los intro va a ser contenido estatico
+// Debes configurar cual va  a aparecer desde la misma pagina de inicio y luego configurar sus items
+// según la intro escogida
 
 
